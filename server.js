8f15dceb21562123ddfd107f99c833593687300a
unknown <sanketdarwante6@gmail.com>
2020-09-05 23:01:42 +0530

    server config change

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use(express.static('./dist/todoui'));
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: '/dist/todoui'}
+ 
+
+  res.sendFile(path.join(__dirname,'/dist/todoui/index.html')
 );
 });
 
